5a43f30b256f621cb5362094798e05011572be44
Evan McFadden <emcfadden@hueymagoos.com>
2025-10-06 00:52:21 -0400

    Home page aesthetics

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -55,7 +55,7 @@ const AuthProvider = ({ children }: any) => {
 
     const unsubscribe = Hub.listen('auth', listener);
     return () => unsubscribe();
-  }, [dispatch]);
+  }, [dispatch, router]);
 
   const checkAuthState = async () => {
     try {
